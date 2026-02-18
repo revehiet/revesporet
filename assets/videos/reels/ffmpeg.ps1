198abdf8ec5fecbf1926c122cf15cdbfdf638427
revehiet <149735586+revehiet@users.noreply.github.com>
2026-02-18 16:02:30 +0100

    SoMe videoer.

---
+++ assets/videos/reels/ffmpeg.ps1
@@ -0,0 +1,11 @@
+Get-ChildItem *.mp4 | ForEach-Object {
+    $name = $_.BaseName
+    New-Item -ItemType Directory -Force -Path $name | Out-Null
+
+    ffmpeg -i "$($_.FullName)" `
+        -c:v libx264 -preset slow -crf 23 `
+        -c:a aac -b:a 96k `
+        -hls_time 4 -hls_playlist_type vod `
+        -hls_segment_filename "$name\seg_%03d.ts" `
+        "$name\index.m3u8"
+}
